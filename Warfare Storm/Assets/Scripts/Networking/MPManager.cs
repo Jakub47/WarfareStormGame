afe923c5a1fe17592b85e9e2c740dcbe586f5182
Jakub47 <jakub7447@gmail.com>
2019-05-20 22:13:21 +0200

    #4 Joining the random room and creating the random room

---
+++ Warfare Storm/Assets/Scripts/Networking/MPManager.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Photon;
+using Photon.Pun;
+using Photon.Realtime;
+using UnityEngine.SceneManagement;
+
+public class MPManager : MonoBehaviourPunCallbacks
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        PhotonNetwork.ConnectUsingSettings();
+        
+    }
+
+    public override void OnConnectedToMaster()
+    {
+       
+        Debug.Log("We are now connected to Photon!");
+    }
+
+    public void JoinF4A()
+    {
+        PhotonNetwork.AutomaticallySyncScene = true;
+        PhotonNetwork.JoinRandomRoom();
+    }
+
+    public override void OnJoinRandomFailed(short returnCode, string message)
+    {
+        CreateF4A();
+    }
+
+    public void CreateF4A()
+    {
+        PhotonNetwork.AutomaticallySyncScene = true;
+        RoomOptions ro = new RoomOptions { MaxPlayers = 10, IsOpen = true, IsVisible = true };
+        PhotonNetwork.CreateRoom("defaultF4A", ro, TypedLobby.Default);
+
+        SceneManager.LoadScene(3);
+    }
+
+}
