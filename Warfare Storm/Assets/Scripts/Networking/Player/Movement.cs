286b0aa2bdbfe2c489cae7b8cd9b1c81d05129b1
Jakub47 <jakub7447@gmail.com>
2019-05-23 23:10:04 +0200

    $8 IK + Contrain for head

--- Warfare Storm/Assets/Scripts/Networking/Player/Movement.cs
+++ Warfare Storm/Assets/Scripts/Networking/Player/Movement.cs
@@ -17,6 +17,13 @@ public class Movement : MonoBehaviourPun
     private Rigidbody body;
     private Animator anim;
     public GameObject cam;
+    public Transform head;
+    public Vector3 HeadOffset;
+    public Transform headLock;
+    public float AnimationSpeed;
+    public Transform GunHoldOne;
+    public Transform GunHoldTwo;
+
     //public GameObject Head;
     // Start is called before the first frame update
     void Start()
@@ -35,10 +42,11 @@ public class Movement : MonoBehaviourPun
         //}
     }
 
-    //private void LateUpdate()
-    //{
-    //    Head.transform.position = Vector3.zero;
-    //}
+    private void LateUpdate()
+    {
+        Quaternion rot = Quaternion.Euler(headLock.transform.eulerAngles.x, headLock.transform.eulerAngles.y, headLock.transform.eulerAngles.z) * Quaternion.Euler(HeadOffset);
+        head.transform.rotation = rot;
+    }
 
     // Update is called once per frame
     void Update()
@@ -50,7 +58,7 @@ public class Movement : MonoBehaviourPun
 
             if (Input.GetKey(Left))
             {
-                anim.SetFloat("Move", .5f);
+                anim.SetFloat("Move", Mathf.Lerp(anim.GetFloat("Move"), .5f, AnimationSpeed * Time.deltaTime));
                 body.AddRelativeForce(Vector3.left * moveSpeed * Time.deltaTime, ForceMode.Impulse);
             }
             else if (Input.GetKeyUp(Left))
@@ -59,7 +67,7 @@ public class Movement : MonoBehaviourPun
             }
             if (Input.GetKey(Right))
             {
-                anim.SetFloat("Move", .5f);
+                anim.SetFloat("Move", Mathf.Lerp(anim.GetFloat("Move"), .5f, AnimationSpeed * Time.deltaTime));
                 body.AddRelativeForce(Vector3.left * -moveSpeed * Time.deltaTime, ForceMode.Impulse);
             }
             else if (Input.GetKeyUp(Right))
@@ -68,7 +76,7 @@ public class Movement : MonoBehaviourPun
             }
             if (Input.GetKey(Forward))
             {
-                anim.SetFloat("Move", .5f);
+                anim.SetFloat("Move", Mathf.Lerp(anim.GetFloat("Move"), .5f, AnimationSpeed * Time.deltaTime));
                 body.AddRelativeForce(Vector3.forward * moveSpeed * Time.deltaTime, ForceMode.Impulse);
             }
             else if (Input.GetKeyUp(Forward))
@@ -77,24 +85,35 @@ public class Movement : MonoBehaviourPun
             }
             if (Input.GetKey(Backward))
             {
-                anim.SetFloat("Move", .5f);
+                anim.SetFloat("Move", Mathf.Lerp(anim.GetFloat("Move"), .5f, AnimationSpeed * Time.deltaTime));
                 body.AddRelativeForce(Vector3.forward * -moveSpeed * Time.deltaTime, ForceMode.Impulse);
             }
             else if (Input.GetKeyUp(Backward))
             {
-                anim.SetFloat("Move", 0);
+                anim.SetFloat("Move",0);
             }
             if (Input.GetKey(KeyCode.LeftShift))
             {
-                anim.SetFloat("Move", 1f);
+                anim.SetFloat("Move", Mathf.Lerp(anim.GetFloat("Move"), 1f, AnimationSpeed * Time.deltaTime));
                 body.AddRelativeForce(Vector3.left * (moveSpeed + 30) * Time.deltaTime, ForceMode.Impulse);
             }
             else if (Input.GetKeyUp(KeyCode.LeftShift))
             {
-                anim.SetFloat("Move", 0f);
+                anim.SetFloat("Move", 0);
             }
             gameObject.transform.Rotate(new Vector3(0, x, 0));
             cam.transform.Rotate(new Vector3(-y, 0, 0));
         }
+    }
+
+    private void OnAnimatorIK(int layerIndex)
+    {
+        anim.SetIKPositionWeight(AvatarIKGoal.RightHand, 1);
+        
+        anim.SetIKPosition(AvatarIKGoal.RightHand, GunHoldOne.transform.position);
+
+      //  anim.SetLookAtWeight(1);
+      //  anim.SetLookAtPosition(cam.transform.position);
+      
     }
 }
